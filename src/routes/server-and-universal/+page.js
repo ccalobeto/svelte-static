0c6b6fe6e8ee548c7d61a99aacd35e2638709e24
ccalobeto <calobeto.leon@gmail.com>
2025-01-11 17:37:59 -0500

    1.6 Server and Universal route

---
+++ src/routes/server-and-universal/+page.js
@@ -0,0 +1,8 @@
+export const load = async () => {
+	return {
+		number: Math.random(),
+	};
+};
+
+export const prerender = true;
+// export const ssr = false;
---
+++ src/routes/server-and-universal/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let data;
+</script>
+
+<h1>
+	A (maybe?) random number: {data.number}
+</h1>
