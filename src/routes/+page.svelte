90b984ea1865d5d4455e53ee774f7b00698ba245
ccalobeto <calobeto.leon@gmail.com>
2025-01-12 17:50:00 -0500

    add routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>Visit <a href="./prerendered">prerendered</a></p>
-<p>Visit <a href="./dynamic">dynamic</a></p>
+<h1>Menu of adapter-static</h1>
+<p>Visit <a href="./prerendered">prerendered route</a></p>
+<p>Visit <a href="./dynamic">dynamic route</a></p>
+<p>Visit <a href="./random-number.json">random-number route</a></p>
+<p>Visit <a href="./server-load">server-load route</a></p>
+<p>Visit <a href="./server-and-universal">server-and-universal route</a></p>
+<p>Visit <a href="./ssr">ssr route</a></p>
